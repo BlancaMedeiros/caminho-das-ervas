11875178e896fc56a7915b165f353b18d3eb1a4a
BlancaMedeiros <biancamedeiroslabiapari@gmail.com>
2026-05-17 12:28:14 -0300

    feat: Ajuste na mensagem do WhatsApp

--- src/app/components/pagamento/pagamento.component.ts
+++ src/app/components/pagamento/pagamento.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { CarrinhoService } from '../../services/carrinho.service';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { carrinhoModel } from '../../models/carrinho.model';
 
 @Component({
   selector: 'app-pagamento',
@@ -16,14 +17,15 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
   pagamentoRealizado: boolean = false;
   
   // Variáveis para armazenar o resumo pós-venda
-  itensPedido: any[] = [];
+  itensPedido: carrinhoModel[] = [];
   totalPedido: number = 0;
   enderecoEntrega: string = '';
 
   formularioPagamento = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
     cpf: new FormControl('', [Validators.required, Validators.pattern(/^\d{11}$/)]),
-    endereco: new FormControl('', [Validators.required])
+    endereco: new FormControl('', [Validators.required]),
+    metodo: new FormControl('pix', [Validators.required])
   });
 
   constructor(public carrinhoService: CarrinhoService) {}
@@ -42,7 +44,13 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
   }
 
   concluirNoWhatsApp() {
-    const mensagem = `Olá! Gostaria de confirmar meu pedido.\nTotal: R$ ${this.totalPedido.toFixed(2)}\nEndereço: ${this.enderecoEntrega}`;
+    let listaprodutos = "";
+    this.itensPedido.forEach(item=>{
+      listaprodutos+= `${item.quantidade}x ${item.produto.nome}\n`
+    })
+
+    
+    const mensagem = `Olá! Gostaria de confirmar meu pedido.\nTotal: R$ ${this.totalPedido.toFixed(2)}\nForma de pagamento: ${this.formularioPagamento.value.metodo || ''}\nEndereço: ${this.enderecoEntrega}\nProdutos: ${listaprodutos}`;
     const url = `https://wa.me/5511959447449?text=${encodeURIComponent(mensagem)}`;
     window.open(url, '_blank');
   }
